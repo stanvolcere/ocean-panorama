ca8ccf237bb833817f516585d5fed4e9fec40bb2
stanvolcere <stanvolcere@hotmail.com>
2019-08-07 00:02:05 +0100

    improved styling to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,7 +5,17 @@ class Landing extends Component {
     return (
       <div>
         <div className="landing-hero" />
-        <div className="text-box" />
+        <div className="text-box">
+          <div className="landing__page__heading-primary">
+            <div className="heading-primary-main">
+              Ocean Panorama
+            </div>
+            <div className="heading-primary-sub">
+              Sunny Holidays in the Seychelles
+            </div>
+          </div>
+        </div>
+        <div className="landing-hero" />
       </div>
     );
   }
