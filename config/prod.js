1fdfb747540c69609a5ad3b065549ad4045dbe80
stanvolcere <stanvolcere@hotmail.com>
2019-09-02 02:36:27 +0100

    added config for prod

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    facebookAppID: process.env.FACEBOOK_APP_ID,
+    facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    sendgridApiKey: process.env.SENDGRID_API_KEY,
+    jwtSecret: process.env.JWT_SECRET,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+};
