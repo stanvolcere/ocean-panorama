b1fc860ca2d4cc2d566e5f7693ebc9fcbdeed8d8
stanvolcere <stanvolcere@hotmail.com>
2019-10-25 23:15:38 +0100

    added host/guests message component

---
+++ client/src/components/InstantMessage/InstantMessage.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import requireAuth from "../utils/requireAuth";
+
+class InstantMessage extends Component {
+
+    renderMessageBox() {
+        return <div className="field">
+
+            <textarea></textarea>
+        </div>
+    }
+
+    renderMessageDisplay() {
+        return <div>message display</div>
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="content__heading">Message Host</div>
+                <div className="instant__message__container">
+                    <div className="instant__message__box">
+                        {this.renderMessageBox()}
+                    </div>
+                    <div className="instant__message__box">
+                        {this.renderMessageDisplay()}
+                    </div>
+                </div>
+            </div >
+        );
+    }
+}
+
+const mapStateToProps = ({ auth }) => {
+    return {
+        auth
+    };
+};
+
+export default connect(mapStateToProps, null)(requireAuth(InstantMessage));
